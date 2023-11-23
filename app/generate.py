64bf7cf86a6f83455dcf06c0043a23c48e9edd88
daviamarall <davi.sdk@hotmail.com>
2023-11-23 16:22:21 -0300

    commit

--- app/generate.py
+++ app/generate.py
@@ -3,12 +3,26 @@ import os
 import markdown2
 from jinja2 import Environment, FileSystemLoader
 
+# Obter o caminho absoluto do diretório do script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Caminho completo para o diretório de templates
+template_dir = os.path.join(script_dir, "templates")
+
+# Caminho completo para o arquivo JSON
+json_file_path = os.path.join(script_dir, "data.json")
+
+# Verificar se o arquivo JSON existe
+if not os.path.exists(json_file_path):
+    print(f"Erro: Arquivo JSON não encontrado - {json_file_path}")
+    exit()
+
 # Carregar dados do arquivo JSON
-with open("data.json", "r") as file:
+with open(json_file_path, "r") as file:
     data = json.load(file)
 
 # Configurar o ambiente Jinja2
-env = Environment(loader=FileSystemLoader("templates"))
+env = Environment(loader=FileSystemLoader(template_dir))
 index_template = env.get_template("index.html")
 post_template = env.get_template("post.html")
 
@@ -16,9 +30,9 @@ post_template = env.get_template("post.html")
 for post in data:
     # Usar o título da postagem como parte do caminho do arquivo Markdown
     title_for_filename = post.get("title", "").lower().replace(' ', '_')
-    markdown_file_path = f"mddir/{title_for_filename}.md"
+    markdown_file_path = os.path.join(script_dir, f"mddir/{title_for_filename}.md")
 
-    # Ler conteúdo do arquivo Markdown
+    # Verificar se o arquivo Markdown existe
     if os.path.exists(markdown_file_path):
         with open(markdown_file_path, "r", encoding="utf-8") as md_file:
             markdown_content = md_file.read()
@@ -35,7 +49,7 @@ for post in data:
         )
 
         # Salvar a postagem como um arquivo HTML
-        output_filename = f"output/{post['title'].replace(' ', '_')}.html"
+        output_filename = os.path.join(script_dir, f"output/{post['title'].replace(' ', '_')}.html")
         with open(output_filename, "w", encoding="utf-8") as output_file:
             output_file.write(rendered_post)
 
@@ -45,7 +59,8 @@ for post in data:
 
 # Renderizar a página inicial
 rendered_index = index_template.render(posts=data)
-with open("output/index.html", "w", encoding="utf-8") as index_file:
+output_index_path = os.path.join(script_dir, "output/index.html")
+with open(output_index_path, "w", encoding="utf-8") as index_file:
     index_file.write(rendered_index)
 
 print("Blog atualizado com sucesso!")
